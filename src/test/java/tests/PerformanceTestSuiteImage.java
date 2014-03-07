7275df18f3d740d8b1ff7549e96cb0f63092c078
Ian Schneider <planablediglet@gmail.com>
2014-03-07 11:37:52 -0700

    add warm up and lower number of requests

--- src/test/java/tests/PerformanceTestSuiteImage.java
+++ src/test/java/tests/PerformanceTestSuiteImage.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +19,20 @@ public class PerformanceTestSuiteImage extends BaseTest {
 
     static {
         scenario.reporter().toggleCSVReportingMode();
+        tests.setReporter(null);
+        for (int i = 0; i < 3; i++) {
+            try {
+                tests.getFeatureAsImage(VA_ROADS, VA_ROADS.getAssociatedStyles().get(0));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        tests.setReporter(scenario.reporter());
     }
 
     @Test
     public void testPerformanceFeatureFullImage() throws Exception {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
             tests.getFeatureAsImage(VA_ROADS, VA_ROADS.getAssociatedStyles().get(0));
         }
     }
@@ -28,7 +40,7 @@ public class PerformanceTestSuiteImage extends BaseTest {
     @Test
     public void testPerformanceFeaturePartialImage1() throws Exception {
         // 1023 features
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
             tests.getFeatureAsImage(VA_ROADS, VA_ROADS.getAssociatedStyles().get(0),
                 "bbox", "-77.7759011555055,38.73029438222257,-74.64079463398436,41.865400903743705");
         }
@@ -37,7 +49,7 @@ public class PerformanceTestSuiteImage extends BaseTest {
     @Test
     public void testPerformanceFeaturePartialImage2() throws Exception {
         // 742 features
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
             tests.getFeatureAsImage(VA_ROADS, VA_ROADS.getAssociatedStyles().get(0),
                 "bbox", "-77.7759011555,38.7302943822,-76.2083478947,40.297847643");
         }
@@ -46,7 +58,7 @@ public class PerformanceTestSuiteImage extends BaseTest {
     @Test
     public void testPerformanceFeaturePartialImage3() throws Exception {
         // 259
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
             tests.getFeatureAsImage(VA_ROADS, VA_ROADS.getAssociatedStyles().get(0),
                 "bbox", "-77.7759011555,38.7302943822,-76.9921245251,39.5140710126");
         }
